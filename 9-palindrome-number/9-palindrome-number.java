68d08e0498e56379237bf83fe0611eb433cb5f2c
Lewis Wilson <55784291+lewisjwilson@users.noreply.github.com>
2022-09-28 23:59:19 +0900

    Time: 9 ms (97.64%), Space: 47.3 MB (9.54%) - LeetHub

--- 9-palindrome-number/9-palindrome-number.java
+++ 9-palindrome-number/9-palindrome-number.java
@@ -1,5 +1,6 @@
 class Solution {
     public boolean isPalindrome(int x) {
+        if(x<0){return false;}
         String xStr = String.valueOf(x);
         String xRev = new StringBuilder().append(xStr).reverse().toString();
         return xStr.equals(xRev) ? true : false;
